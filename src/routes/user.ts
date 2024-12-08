3a3dd6829fedf99625c07633fa73733f976fae7e
Albin David C <albindavidc007@gmail.com>
2024-12-08 18:55:44 +0530

    created admin side views and updated admin controller

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {registerUser, loginUser, logoutUser} from "../controllers/libraryController";
 import { ensureAuthenticated } from "../middleware/authMiddleware";
 import { Request, Response, NextFunction } from "express";
+import { getAdminDashboard, addBook, assignBook } from "../controllers/adminController";
 
 const router = express.Router();
 
@@ -11,6 +12,8 @@ router.post("/login", loginUser);
 router.post("/register", registerUser);
 router.post("/logout", ensureAuthenticated, logoutUser);
 
-
+router.get('/dashboard', getAdminDashboard);
+router.post('/admin/add-book', ensureAuthenticated, addBook);
+router.post('/admin/assign-book', ensureAuthenticated, assignBook);
 
 export default router;
